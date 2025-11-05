d8549466e1dc377532ac4732cd5528a1d126cdac
Your Name <your@email.com>
2025-11-04 21:02:51 -0500

    temples

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+// temples.js - Temple album specific JavaScript functionality
+// Author: Michael Smith Saavedra Forero
+
+// Example: Display a custom message in the console
+document.getElementById("currentyear").textContent = new Date().getFullYear();
