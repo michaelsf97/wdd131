88a5a6c0b3859af919687979b058a3a7b274bfa9
Your Name <your@email.com>
2025-11-05 21:06:38 -0500

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,15 +4,17 @@
 // Example: Display a custom message in the console
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
+
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
-}
-
-if (navMenu.classList.contains("active")) {
-    hamburger.textContent = "X";
-} else {
-    hamburger.textContent = "☰";
+    
+    
+    if (navMenu.classList.contains("active")) {
+        hamburger.textContent = "✕";
+    } else {
+        hamburger.textContent = "☰";
+    }
 });
